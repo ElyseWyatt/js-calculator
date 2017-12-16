72612d2df65bed9933590e78a64e3422228ec838
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2017-12-16 16:38:14 +1300

    Edits.

--- calculator.js
+++ calculator.js
@@ -8,21 +8,46 @@ document.addEventListener('DOMContentLoaded', function () {
 //this event listener goes first. It tells all the files to load all the html content before anymore JavaScript is run.
 
 var answer = ""
-var buttons = document.getElementsByTagName("button");
-var operators = ['+','-','x','%'];
-var decimalAdded = false;
+var numbers = [];
+
+//var buttons = document.getElementsByTagName("button");
+
 
 //2. For each button, add an onclick event addEventListener. This works by attaching a listener to the button 'tags' in the html (anything with <button></button>). I could have used 'getElementsByClassName' also. This whole equation is selecting all button elements and saving into a variable with var buttons.
 
+var buttons = document.getElementById('buttons').getElementsByTagName('button')
 
 for (var i = 0; i < buttons.length; i++) {
-  var button = buttons[i]
-button.addEventListener("click", function(e) {
-  answer = answer + e.target.value;
-  console.log(answer)
+   var button = buttons[i]
+   button.addEventListener("click", handleClick)
 }
 
-)}
+function handleClick(e) {
+   var value = e.target.value
+  answer += e.target.value;
+ document.getElementById('display').innerHTML = answer
+
+ //if event target is "+" then don't add to array.
+
+ if (e.target.value === '=') {
+   var sum = Number(numbers[0]) + Number(numbers[1])
+   document.getElementById('answer').innerHTML = sum;
+
+ }
+
+  if (value !== "+" && value !== "=") {
+    numbers.push(value);
+  }
+
+
+}
+
+
+
+// going to put getelementbyid then innerHTML
+
+//var operators = ['+','-','x','%'];
+//var decimalAdded = false;
 
 //3. Value of button
 //Now we want to make something happen when we click on the buttons. At the moment, when each button is clicked it is printing each number on the console. When I click 2, it prints 2. When I click 3, it then prints 23.
